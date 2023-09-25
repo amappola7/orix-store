1b8378c0e33dd883040610aa0b705561317fd39b
Amappola <anam.porrasp@gmail.com>
2023-09-25 09:29:55 -0500

    Creating the product service and the method getProdcucts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { IProduct } from '../interfaces/iproduct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private _url: string = 'https://fakestoreapi.com/products';
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  getProducts(): Observable<IProduct[]> {
+    return this._http.get<IProduct[]>(this._url);
+  }
 }
