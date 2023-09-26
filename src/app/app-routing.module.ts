810d9b6202b4b0e746082a9f34b1c4418b88dbf5
Amappola <anam.porrasp@gmail.com>
2023-09-26 15:09:32 -0500

    Setting up the routes for the components log in, sign up and shopping cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   {path: '', component: AppComponent},
+  {path: 'log-in', component: LoginComponent},
+  {path: 'sign-up', component: SignupComponent},
+  {path: 'shopping-cart', component: ShoppingCartComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
