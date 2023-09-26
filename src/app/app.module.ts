506fb4f5d7bb6e7f04606486adc6307d0ed4dd22
Amappola <anam.porrasp@gmail.com>
2023-09-25 21:22:57 -0500

    Creating the header component and fixing some styles in the global styles and the card component styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CardComponent } from './components/card/card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CardComponent
+    CardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { faBars, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  screenSize: number = window.screen.width;
+  icons = {
+    barsMenuIcon: faBars,
+    shoppingCartIcon: faShoppingCart
+  };
+
+}
