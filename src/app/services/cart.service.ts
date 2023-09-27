5795c2f372ddcbf1049c00be5769386a8f858be9
Amappola <anam.porrasp@gmail.com>
2023-09-27 16:43:11 -0500

    Creating the add product methos in the cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { ICartItem } from '../interfaces/icart-item';
+import { IProduct } from '../interfaces/iproduct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cart: ICartItem[]  = [];
 
   constructor() { }
+
+  addProduct(productToAdd: IProduct, quantityToAdd: number): void {
+    const item: ICartItem = {
+      quantity: quantityToAdd,
+      product: productToAdd
+    };
+
+    this.cart.push(item);
+  }
 }
