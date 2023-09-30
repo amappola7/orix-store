64cf3d88403a37047c801da8fe5ef1d0ef306d84
Amappola <anam.porrasp@gmail.com>
2023-09-29 19:17:03 -0500

    Creating the layout for the admin view component and defining its styles

--- src/app/components/admin-view/admin-view.component.ts
+++ src/app/components/admin-view/admin-view.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/iproduct';
+import { ProductService } from 'src/app/services/product.service';
 import { ScreenModeService } from 'src/app/services/screen-mode.service';
+import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-admin-view',
@@ -8,13 +11,22 @@ import { ScreenModeService } from 'src/app/services/screen-mode.service';
 })
 export class AdminViewComponent {
   screenMode!: boolean;
+  products: IProduct[] = [];
+  icons = {
+    editIcon: faPencil,
+    deleteIcon: faTrash
+  };
+  screenSize: number = window.screen.width;
 
   constructor(
-    private screenModeService: ScreenModeService
+    private screenModeService: ScreenModeService,
+    private productService: ProductService
   ) {}
 
   ngOnInit() {
     this.screenMode = this.screenModeService.screenMode;
+    this.productService.getProducts()
+    .subscribe((products) => this.products = products);
   }
 
   setScreenMode(mode: boolean): void {
