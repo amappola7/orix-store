2dd242d0d22f8488f693b27d2318120ad6826445
Amappola <anam.porrasp@gmail.com>
2023-10-03 10:20:01 -0500

    Cleaning the service by deleting unused variables

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { IProduct } from '../interfaces/iproduct';
 
 @Injectable({
