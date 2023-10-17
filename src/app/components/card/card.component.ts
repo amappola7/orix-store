cfe8b0c12bf5bac042febf9ac6bd563194473c73
Amappola <anam.porrasp@gmail.com>
2023-10-17 14:26:29 -0500

    Fixing some styles

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { IProduct } from 'src/app/interfaces/iproduct';
 import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
+import { faShoppingCart, faInfo } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-card',
@@ -9,6 +10,10 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+  icons = {
+    shoppingCartIcon: faShoppingCart,
+    moreInfoIcon: faInfo
+  };
   selectedProduct!: IProduct;
   @Input() productInfo!: IProduct;
   @Input () inputScreenModeII!: boolean;
