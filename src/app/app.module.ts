5a41b8e5c799a2ab70af1e1ce11c21241afce64e
Amappola <anam.porrasp@gmail.com>
2023-12-15 09:48:12 -0500

    Moving the home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './ui/pages/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CardComponent } from './components/card/card.component';
 import { HeaderComponent } from './components/header/header.component';
